cc5403a2361f805824bce6b6d7b2cb06b189d925
Jaroš Václav <va.jaros@gmail.com>
2025-06-12 08:55:01 +0200

    Přidává hlavní řídicí soubor.

---
+++ main.py
@@ -0,0 +1,9 @@
+from assembly_line_reader import run_assemly_line_reader
+
+def main():
+    """ Main entry point for the script."""
+    
+    run_assemly_line_reader()
+    
+if __name__ == "__main__":
+    main()
